aead1e59bbb268518cd2f15f6547bbdaf95e77d5
superman-lopez <43129263+superman-lopez@users.noreply.github.com>
2019-03-21 13:01:46 +0800

    Implemented API resource functionality
    The API call is basis two characters, further filtering is done in the array

--- auto-complete.component.ts
+++ auto-complete.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ViewEncapsulation, ElementRef } from '@angular/core';
 import { ICellEditorAngularComp } from 'ag-grid-angular';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -15,6 +16,8 @@ import { ICellEditorAngularComp } from 'ag-grid-angular';
         border: none;
         height: 500px;
         overflow: visible;
+        left: 250px;
+        top: 10px;
     }
     .auto-complete-input-field-balham {
         outline: initial;
@@ -38,7 +41,7 @@ import { ICellEditorAngularComp } from 'ag-grid-angular';
         class="auto-complete-input-field-balham" 
         [style.width]="params.column.actualWidth + 'px'">
     <ag-grid-angular
-        style="width: 850px; max-height: 200px; font-weight: 200;" 
+        style="height: 200px; font-weight: 200;" 
         class="ag-theme-balham"
         [rowData]="rowData" 
         [columnDefs]="columnDefs"
@@ -56,19 +59,25 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
     private params: any;
     private gridApi: any;
     private rowData: any;
-    private columnDefs: any;
+    private columnDefs: [{}];
     private rowSelection: string = 'single';
     // variables for component
     private returnObject: boolean;
     private cellValue: string;
     private filteredRowData: any;
     private inputValue: string;
+    private apiEndpoint: string;
+    private useApi: boolean;
     private propertyName: string;
-    private isCanceled: boolean = true;
+    private isCanceled: boolean;
     private selectedObject: any = {
       }
 
     @ViewChild("input") input: ElementRef;
+
+    constructor(private httpClient: HttpClient) {
+        
+    }
 
  
     // below puts focus in input field at start of cellEditor, or selects if field had a previous value
@@ -85,8 +94,13 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
     // ICellEditorAngularComp functions
     agInit(params: any): void {
         this.params = params;
-
-        this.rowData = params.rowData;
+        if (!params.rowData) {
+            this.apiEndpoint = params.apiEndpoint;
+            this.useApi = true;
+            this.rowData = [{[params.propertyRendered]: 'Type at least 2 characters'}]
+        } else {
+            this.rowData = params.rowData;
+        }
         this.columnDefs = params.columnDefs;
         this.propertyName = params.propertyRendered;
         this.cellValue = params.value[this.propertyName];
@@ -99,9 +113,10 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
         }
     }
 
+    
     getValue(): any {
-       if (!this.returnObject) return this.selectedObject[this.propertyName];
-       return this.selectedObject;
+        if (!this.returnObject) return this.selectedObject[this.propertyName];
+        return this.selectedObject;
     }
     isPopup(): boolean {
         return true;
@@ -109,7 +124,7 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
     isCancelAfterEnd(): boolean {
         return this.isCanceled
     }
-
+    
     // ag-Grid functions
     onGridReady(params) {
         this.gridApi = params.api;
@@ -122,7 +137,7 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
         this.isCanceled = false;
         this.params.api.stopEditing();
     }
-
+    
     rowConfirmed() {
         if(this.gridApi.getSelectedRows()[0]) {
             this.selectedObject = this.gridApi.getSelectedRows()[0];
@@ -130,7 +145,7 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
         }
         this.params.api.stopEditing(); 
     }
-
+    
     onKeydown(event) {
         event.stopPropagation();
         if (event.key == "Escape") {
@@ -145,23 +160,46 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
             this.navigateGrid();
             return false;
         } 
+        this.processDataInput(event)
+    }
+    
+    processDataInput(event) {
         window.setTimeout(() => {
-            this.updateFilter(this.inputValue.toLowerCase());
+            if (this.useApi && this.inputValue.length == 2 && event.key != "Backspace") {
+                this.getApiData(this.inputValue).subscribe(data => {
+                    this.rowData = data;
+                    this.updateFilter(this.inputValue.toLowerCase());
+                });
+            } else {
+                this.updateFilter(this.inputValue.toLowerCase());
+            }
         })
+    }
+    
+    getApiData(filter) {
+        return this.httpClient.get(this.apiEndpoint + this.toQueryString(filter));
+    }
+    
+    toQueryString(filter) {
+        return "?" + this.propertyName + "=" + filter;
     }
 
     updateFilter(value) {
-        console.log(value);
         this.filteredRowData = this.rowData.filter(row => 
             row[this.propertyName].toLowerCase().includes(value))
             .sort((a, b) => {
-            if(a[this.propertyName] < b[this.propertyName]) { return -1; }
-            if(a[this.propertyName] > b[this.propertyName]) { return 1; }
-            return 0;});
-        this.gridApi.setRowData(this.filteredRowData);
-        if(this.filteredRowData[0]) this.gridApi.getRowNode(0).setSelected(true);
+                if(a[this.propertyName] < b[this.propertyName]) 
+                    { return -1; };
+                if(a[this.propertyName] > b[this.propertyName]) 
+                    { return 1; };
+                return 0;});
+                this.gridApi.setRowData(this.filteredRowData);
+                if(this.filteredRowData[0]) {
+                    this.gridApi.getRowNode(0).setSelected(true);
+                }
+
     }
- 
+
     navigateGrid() {
         if(!this.gridApi.getFocusedCell()) {
             this.gridApi.setFocusedCell(0, this.propertyName);
