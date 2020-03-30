798d0291f67acb322d52041d9008be62a12ef76f
superman-lopez <43129263+superman-lopez@users.noreply.github.com>
2020-03-30 10:54:25 +0800

    Updated change detection and overlay messages

--- auto-complete.component.ts
+++ auto-complete.component.ts
@@ -1,16 +1,19 @@
-import { Component, AfterViewInit, ViewChild, ViewEncapsulation, ElementRef, HostListener } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewEncapsulation, ElementRef, HostListener, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { ICellEditorAngularComp } from 'ag-grid-angular';
 import { HttpClient } from '@angular/common/http';
+import { GridReadyEvent, RowClickedEvent, ColDef, GridApi } from 'ag-grid-community';
 
 
 @Component({
 	selector: 'auto-complete',
 	encapsulation: ViewEncapsulation.None,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: { style: `position: absolute;
 					left: 0px; 
 					top: 0px;
-					background-color: transparant;
-					` },
+					overflow: visible;
+					`
+	},
     template: ` 
 		<input #input
 			[(ngModel)]="inputValue"
@@ -25,25 +28,29 @@ import { HttpClient } from '@angular/common/http';
 			[rowData]="rowData" 
 			[columnDefs]="columnDefs"
 			[rowSelection]="rowSelection"
+			[overlayNoRowsTemplate]="overlayNoRowsTemplate"
 			(gridReady)="onGridReady($event)"
 			(rowClicked)="rowClicked($event)">
 		</ag-grid-angular>
 	`
 })
-export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewInit {
+export class AgGridAutoCompleteComponent implements ICellEditorAngularComp, AfterViewInit, OnDestroy {
 	// variables for agGrid
 	public params: any;
-	public gridApi: any;
-	public rowData: any;
-	public columnDefs: [{}];
+	public gridApi: GridApi;
+	public rowData: Array<any> = [];
+	public columnDefs: Array<ColDef>;
 	public rowSelection: string = 'single';
 	public columnFilter: any;
+	public overlayNoRowsTemplate: string; 
 	// variables for component
 	public returnObject: boolean;
+	public clearInputValue: boolean;
 	public cellValue: string;
 	public filteredRowData: any;
 	public inputValue: string;
 	public apiEndpoint: string;
+	public queryChars: number = 2;
 	public gridHeight: number = 175;
 	public gridWidth: number = 375;
 	public useApi: boolean;
@@ -53,16 +60,11 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 
     @ViewChild("input") input: ElementRef;
 
-    constructor(private httpClient: HttpClient) {}
-
+    constructor(private httpClient: HttpClient, private changeDetection: ChangeDetectorRef) {}
  
 	ngAfterViewInit() {
 		window.setTimeout(() => {
-			if (this.inputValue == this.cellValue) {
-				this.input.nativeElement.select();
-			} else {
-				this.input.nativeElement.focus();
-			}
+			(this.inputValue == this.cellValue) ? this.input.nativeElement.select() : this.input.nativeElement.focus();
 			if (this.inputValue && !this.useApi) this.updateFilter();
 		})
 	}
@@ -73,22 +75,45 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 		if (!params.rowData) {
 			this.apiEndpoint = params.apiEndpoint;
 			this.useApi = true;
-			this.rowData = [{}]
 		} else {
 			this.rowData = params.rowData;
 		}
 		if (params.gridHeight) this.gridHeight = params.gridHeight;
 		if (params.gridWidth) this.gridWidth = params.gridWidth;
+
+		if (params.queryChars > -1) this.queryChars = params.queryChars;
 		this.columnDefs = params.columnDefs;
 		this.propertyName = params.propertyRendered;
-		this.cellValue = params.value[this.propertyName];
 		this.returnObject = params.returnObject;
-
+		this.clearInputValue = params.clearInputValue;
+		
+		this.cellValue = (params.propertyRendered == '' || params.returnObject == false || params.value == null) ? '' : params.value[this.propertyName];
+		
+		if (this.queryChars == 0) {
+			this.overlayNoRowsTemplate = this.overLayLoading();
+		} else {
+			this.overlayNoRowsTemplate = this.overLayMinimumCharacters();
+		}
+		
 		if (!params.charPress) {
-			if(this.cellValue) this.inputValue = this.cellValue;
+			if(this.cellValue && !this.clearInputValue) this.inputValue = this.cellValue;
 		} else {
 			this.inputValue = params.charPress;
 		}
+
+		if (this.queryChars == 0 || (this.inputValue != null && this.inputValue != '' && this.inputValue.length > this.queryChars)) {
+			this.getApiData(this.inputValue).subscribe(data => { 
+				this.rowData = data as Array<ColDef>;
+				this.changeDetection.detectChanges();
+				window.setTimeout(() => {this.updateFilter()});
+			});
+		} else {
+			this.changeDetection.detectChanges();
+		};
+	}
+
+	ngOnDestroy() {
+		this.changeDetection.detach();
 	}
 
 	getValue(): any {
@@ -103,20 +128,20 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 	}
 
 	// ag-Grid functions
-	onGridReady(params) {
-		this.gridApi = params.api;
+	onGridReady(event: GridReadyEvent): void {
+		this.gridApi = event.api;
 		this.gridApi.sizeColumnsToFit();
 		this.columnFilter = this.gridApi.getFilterInstance(this.propertyName);
 	}
 
 	// component functions
-	rowClicked(params) {
-		this.selectedObject = params.data;
+	rowClicked(event: RowClickedEvent): void {
+		this.selectedObject = event.data;
 		this.isCanceled = false;
 		this.params.api.stopEditing();
 	}
 
-	rowConfirmed() {
+	rowConfirmed(): void {
 		if(this.gridApi.getSelectedRows()[0]) {
 			this.selectedObject = this.gridApi.getSelectedRows()[0];
 			this.isCanceled = false;
@@ -141,16 +166,19 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 		} 
 	}
 
-	processDataInput(event) {
+	processDataInput(inputValue: string): void {
 		if (this.useApi) {
-			if (event.length == 0) this.gridApi.setRowData();
-			if (event.length == 2) {
-				this.getApiData(event).subscribe(data => { 
-					this.rowData = data;
-					setTimeout(() => {this.updateFilter()});
+			if (inputValue.length < this.queryChars) {
+				this.gridApi.setRowData([]);
+			}
+			if (inputValue.length == this.queryChars) {
+				this.getApiData(inputValue).subscribe(data => { 
+					this.rowData = data as Array<ColDef>;
+					this.changeDetection.detectChanges();
+					window.setTimeout(() => {this.updateFilter()});
 				});
 			};
-			if (event.length > 2) this.updateFilter();
+			if (inputValue.length > this.queryChars) this.updateFilter();
 		} else {
 			this.updateFilter();
 		}
@@ -160,25 +188,28 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 		return this.httpClient.get(this.apiEndpoint + this.toQueryString(filter));
 	}
 
-	toQueryString(filter) {
+	toQueryString(filter: string): string {
 		return "?" + this.propertyName + "=" + filter;
 	}
 
-	updateFilter() {
-		this.columnFilter.setModel({
-			type: 'startsWith',
-			filter: this.inputValue,
-		});
-		this.columnFilter.onFilterChanged();
-		if(this.gridApi.getDisplayedRowAtIndex(0)) {
-			this.gridApi.getDisplayedRowAtIndex(0).setSelected(true);
-			this.gridApi.ensureIndexVisible(0, 'top');
-		} else {
-			this.gridApi.deselectAll();
+	updateFilter(): void {
+		if (this.columnFilter && this.gridApi) {
+			this.columnFilter.setModel({
+				type: 'startsWith',
+				filter: this.inputValue,
+			});
+			this.columnFilter.onFilterChanged();
+		
+			if(this.gridApi.getDisplayedRowAtIndex(0)) {
+				this.gridApi.getDisplayedRowAtIndex(0).setSelected(true);
+				this.gridApi.ensureIndexVisible(0, 'top');
+			} else {
+				this.gridApi.deselectAll();
+			}
 		}
 	}
 
-	navigateGrid() {
+	navigateGrid(): void {
 		if(this.gridApi.getFocusedCell() == null || this.gridApi.getDisplayedRowAtIndex(this.gridApi.getFocusedCell().rowIndex) == null) { // check if no cell has focus, or if focused cell is filtered
 			this.gridApi.setFocusedCell(this.gridApi.getDisplayedRowAtIndex(0).rowIndex, this.propertyName);
 			this.gridApi.getDisplayedRowAtIndex(this.gridApi.getFocusedCell().rowIndex).setSelected(true);
@@ -186,5 +217,12 @@ export class AutoCompleteComponent implements ICellEditorAngularComp, AfterViewI
 			this.gridApi.setFocusedCell(this.gridApi.getFocusedCell().rowIndex, this.propertyName);
 			this.gridApi.getDisplayedRowAtIndex(this.gridApi.getFocusedCell().rowIndex).setSelected(true);
 		}
+	}
+
+	overLayLoading(): string {
+		return '<span class="ag-overlay-no-rows-center">No rows to be shown. <br> Loading...</span>';
+	}
+	overLayMinimumCharacters(): string {
+		return `<span class="ag-overlay-no-rows-center">No rows to be shown. <br> This search field requires at least ${this.queryChars} characters.</span>`
 	}
 }
